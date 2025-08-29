a33ffbbe03edd968b88cc715c6ef14dccf2b8ee6
André Ricardo Monteiro da Rocha <andre.ricardo@sou.unifeob.edu.br>
2025-08-29 12:05:20 -0300

    Ajustes

--- Linguagens/Typescript/01 - basicos/dist/produto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class produto {
-    nome: string;
-    preco: number;
-    constructor(nome: string, preco: number);
-    exibirInfo(): void;
-}
-//# sourceMappingURL=produto.d.ts.map
--- Linguagens/Typescript/01 - basicos/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { produto } from "./produto";
-
-const p1 = new produto("Camiseta", 49.99);
-p1.exibirInfo();
