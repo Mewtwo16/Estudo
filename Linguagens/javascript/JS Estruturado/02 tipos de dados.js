ac6ff68a9b41ab40aed29a360629872fad9836e4
André Ricardo Monteiro da Rocha <andre.ricardo@sou.unifeob.edu.br>
2025-08-30 21:07:20 -0300

    Atualização

---
+++ Linguagens/javascript/JS Estruturado/02 tipos de dados.js
@@ -0,0 +1,29 @@
+/*
+    Entendendo tipos de dados primitivos em JavaScript
+    - String
+    - Number
+    - Boolean
+    - Undefined
+    - Null
+
+    Regras importantes:
+
+    Todas as variaveis são declaradas com let ou const
+    O compilador do JavaScript é capaz de identificar o tipo de dado automaticamente
+    Não existe tipagem forte em JavaScript (ex: Java, C, C++)
+    Tipagem fraca (ex: Python, PHP, Ruby)
+    Tipagem dinâmica (ex: Python, PHP, Ruby)
+    Tipagem fraca e dinâmica (ex: JavaScript, Perl, Lua)
+*/
+
+// Dados primitivos
+let nome = "André";// string
+const sobrenome = 'Ricardo';// string
+let n1 = 5; // number tipo inteiro
+let n2 = 10.5; // number tipo float (número com ponto flutuante)
+const resultado = n1 * n2; // number
+let naoDefinidido; // undefined
+let nulo = null; // nulo
+let booleano = true; // boolean (true ou false)
+
+
