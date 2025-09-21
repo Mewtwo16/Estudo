2ce9a0159e7b7500cf55ff896ea8f0dde764fb5c
André Ricardo Monteiro da Rocha <andre.ricardo@sou.unifeob.edu.br>
2025-09-21 16:26:44 -0300

    att

---
+++ Linguagens/javascript/JS Estruturado/02 logica/09 continue.js
@@ -0,0 +1,18 @@
+/*
+    continue, break
+*/
+
+const n = [1,2,3,4,5,6,7,8,9,10];
+
+for (let numeros of n){
+
+    if(numeros % 2){
+        continue;
+    }
+
+    if(numeros === 8){
+        break;
+    }
+
+    console.log(numeros);    
+}
