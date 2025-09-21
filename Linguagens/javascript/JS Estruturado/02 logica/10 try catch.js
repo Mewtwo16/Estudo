34ced809e1d7fe4a96acbb39191557056e2cc772
André Ricardo Monteiro da Rocha <andrericardo552@gmail.com>
2025-09-21 13:13:26 -0300

    att

---
+++ Linguagens/javascript/JS Estruturado/02 logica/10 try catch.js
@@ -0,0 +1,17 @@
+/*
+    Estrutura try, catch, throw, finally
+ */
+
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') throw new TypeError ('Somente numeros');
+    return x + y;
+}
+
+try {
+    console.log(`tenta fazer a ação que pode retornar erro`);
+} catch (e) {
+    console.log(`tratando erro`);
+} finally {
+    console.log(`Sempre executa`);
+}
+
