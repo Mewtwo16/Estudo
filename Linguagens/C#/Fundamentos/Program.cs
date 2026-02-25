f8a213b39192d463da65ab10a49f3433ad8bb2e6
André Ricardo Monteiro da Rocha <andrericardo552@gmail.com>
2026-02-24 21:05:10 -0300

    att

--- Linguagens/C#/Fundamentos/Program.cs
+++ Linguagens/C#/Fundamentos/Program.cs
@@ -6,25 +6,33 @@ using Fundamentos.Funcoes;
 using Fundamentos.POO;
 using Fundamentos.TryCatch;
 
-namespace Fundamentos {
-    class Program {
-        static void Main(string[] args) {
-            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
-                // Basicos
-                {"Variaveis e output - Fundamentos", Variaveis.Executar},
-                {"Manipulação de dados - Fundamentos", Manipulacao.Executar},
-                {"Operadores - Fundamentos", Operadores.Executar},
-                {"Unarios e Ternarios - Fundamentos", UnariosTernarios.Executar},
-                {"Estruturas de Controle - Fundamentos", EstruturasDeControle.Executar},
-                {"Enum e struct", Estruturas.Executar},
-                // Coleções
-                {"Arrays", Arrays.Executar},
-                {"Listas", Listas.Executar},
-                // Classes e metodos
-                {"Classes - Classes", Classes.Executar},
-            });
 
-                        central.SelecionarEExecutar();
+namespace Fundamentos{
+        class Program{
+                static void Main(string[] args){
+                        var central = new CentralDeExercicios(new Dictionary<string, Action>(){
+                                // Basicos
+                                { "Variaveis e output - Fundamentos", Variaveis.Executar },
+                                { "Manipulação de dados - Fundamentos", Manipulacao.Executar },
+                                { "Operadores - Fundamentos", Operadores.Executar },
+                                { "Unarios e Ternarios - Fundamentos", UnariosTernarios.Executar },
+                                { "Estruturas de Controle - Fundamentos", EstruturasDeControle.Executar },
+                                { "Enum e struct", Estruturas.Executar },
+                                // Coleções
+                                { "Arrays", Arrays.Executar },
+                                { "Listas", Listas.Executar },
+                                { "ArrayList", ColecaoArrayList.Executar },
+                                { "Filas", ColecaoArrayList.Executar },
+                                { "Igualdade", ColecaoArrayList.Executar },
+                                // Classes e metodos
+                                { "Classes", Poo.Executar },
+                                { "Interface", Interfaces.Executar },
+                                // Funcoes Lambda
+                                { "Lambda", Lambda.Executar },
+                                { "Extenção", Extencao.Executar },
+                                // tratamentos de Erros
+                                { "Tratamento de Erros", Tratamentos.Executar },
+                        });
                 }
         }
 }
