e630e4ccb4143bf24df85047f04986b8706fe148
André Ricardo Monteiro da Rocha <134181869+AndreRicardo552@users.noreply.github.com>
2026-04-30 21:32:36 -0300

    Delete Matematica directory
    att

--- Matematica/PreAlgebra/utils/quicksort.c
+++
@@ -1,9 +0,0 @@
-
-#include "quicksort.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-int* quicksort(int* arr, int tam) {
-
-
-}
--- Matematica/PreAlgebra/utils/quicksort.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef QUICKSORT_H
-#define QUICKSORT_H
-
-int* quicksort(int* arr, int tam);
-
-#endif
